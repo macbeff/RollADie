9553fbe7b85bc04237917325b573a2993943d298
Angelo Nardo <30449706+macbeff@users.noreply.github.com>
2017-09-04 12:38:26 -0400

    Add files via upload

---
+++ DieRoll.py
@@ -0,0 +1,14 @@
+SBU = 15
+while SBU == 15:
+    Roll=input("Type 'roll' and hit enter if you would like to roll a die or 'stop' if you would like to stop. ")
+    if Roll=="roll":
+        import random
+        x=random.randint(1,6)
+        y=random.randint(1,6)
+        z=x+y
+        print("You rolled a", x, "and a", y, "for a total of", z)
+    elif Roll=="stop":
+        break
+    else:
+        print("Better luck next time!")
+            
